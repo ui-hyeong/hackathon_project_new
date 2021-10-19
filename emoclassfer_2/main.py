74d07074db70e9cab62ebf0b47dd6c8d11f8f258
jeonguihyeong <jwh4861230@likelion.org>
2021-10-19 16:10:21 +0900

    end

--- emoclassfer_2/main.py
+++ emoclassfer_2/main.py
@@ -22,7 +22,7 @@ DATA = DATA.values.tolist()
 
 
 def main():
-
+    # 설정값들 수정하기 위해 위에 몰아서 썻음
     test_size = 0.3
     random_state = 13
     batch_size = 64
@@ -33,22 +33,29 @@ def main():
     warmup_ratio = 0.1
     log_interval = 200
 
+    # 쿠다 사용 가능 여부 확인
     USE_CUDA = torch.cuda.is_available()
     print(USE_CUDA)
 
+    # gpu 사용 코드
     # device = torch.device('cuda:0' if USE_CUDA else 'cpu')
     device = torch.device('cpu')
     print('학습을 진행하는 기기:', device)
+
+    #pre-training 모델 불러오기
     bertmodel = AutoModel.from_pretrained("monologg/kobert")
     tokenizer = AutoTokenizer.from_pretrained("monologg/kobert")
     sents = [sent for sent, _ in DATA]
     labels = [label for _, label in DATA]
+
+    # stratify를 label로 설정하면 균일하게 섞어서 검증시 좀더 정확하게 할 수 있음
     x_train, x_test, y_train, y_test = train_test_split(sents, labels,
                                                         test_size=test_size,
                                                         shuffle=True,
                                                         stratify=labels,
                                                         random_state=random_state)
 
+    # 데이터 전처리 및 토크나이저 적용
     x_train = Build_X(x_train, tokenizer, device)
     y_train = Build_y(y_train, device)
     x_test = Build_X(x_test, tokenizer, device)
@@ -56,9 +63,11 @@ def main():
 
 
 
-
+    # 파이토치의 dataloader를 사용하기 위한 자연어 강사의 코드.
+    # 파이토치에 dataset이라는 기능이 있는데 공부하기 싫어서 이거 씀
     train_dataset = SimpleDataset(x_train, y_train)
     test_dataset = SimpleDataset(x_test, y_test)
+    # dataloader를 사용하면 알아서 배치사이즈에 맞게 데이터를 출력해서 순차적으로 학습시킬 수 있음
     train_dataloader = DataLoader(dataset=train_dataset,
                                   batch_size=batch_size,
                                   shuffle=True)
@@ -66,19 +75,23 @@ def main():
                                  batch_size=batch_size,
                                  shuffle=True)
 
+    # 학습하기 앞서 모델 정의
     classfer = emoClassifer(bertmodel, num_class=num_class, device=device)
+    # 이건 뭔지 기억이 안나는데 아마 bertAdam을 사용 하기 위한 옵티마이저 설정이었음
     no_decay = ['bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
         {'params': [p for n, p in classfer.named_parameters() if not any(nd in n for nd in no_decay)],
          'weight_decay': 0.01},
         {'params': [p for n, p in classfer.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
+    # 옵티마이저 설정
     optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
     # optimizer = torch.optim.Adam(classfer.parameters(), lr=learning_rate)
 
     t_total = len(train_dataloader) * EPOCHS
     warmup_step = int(t_total * warmup_ratio)
 
+    # 자연어 강사 슬랙에 올라왔던 스케쥴러. 학습률을 변경시켜 좀더 효율적으로 학습시킴.
     scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
 
     #------------------------------ train & test ---------------------------------
--- emoclassfer_2/train_test.py
+++ emoclassfer_2/train_test.py
@@ -12,17 +12,20 @@ def train_test(EPOCHS=None, model=None, train_dataloader=None, test_dataloader=N
         test_f1 = 0.0
         train_f1 = 0.0
 
-        model.train()
+        model.train() # 이거선언해야 모델의 신경망이 수정됨
         for b_idx, batch in enumerate(tqdm(train_dataloader)):
-            optimizer.zero_grad()
+            optimizer.zero_grad() # zero_grad를 선언함으로써 역전파를 통한 기울기를 초기화 시킴
             X, y = batch
-            loss = model.training_step(X, y)
+            loss = model.training_step(X, y) # 손실함수를 통해 loss값을 구함
 
-            loss.backward()  # backprop the loss
+            loss.backward()  # 역전파를 통해 기울기를 구함
+            # 기울기 소실과 폭주 문제를 해결하기 위해 기울기의 절대값이 특정값을 넘어가게 될 경우 기울기의 절대값을 줄여줌
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+
             optimizer.step()  # gradient step
             scheduler.step()  # resetting the gradients.
 
+            # ========== 여기부터 학습 정확도 측정 ===================
             y_hat = model.predict(X)
             y_hat = F.softmax(y_hat, dim=1)
             _, max_indices = torch.max(y_hat, 1)
@@ -34,7 +37,7 @@ def train_test(EPOCHS=None, model=None, train_dataloader=None, test_dataloader=N
                 print("epoch {} batch id {} loss {} train acc {}".format(e_idx+1, b_idx + 1, avg_loss, train_f1/(b_idx+1)))
         print("epoch {} avg_loss {} train acc {} ".format(e_idx+1, avg_loss, train_f1))
 
-        model.eval()
+        model.eval() # 이거 선언해야 신경망이 안바뀜
         for b_idx, batch in enumerate(tqdm(test_dataloader)):
             X, y = batch
             y_hat = model.predict(X)
